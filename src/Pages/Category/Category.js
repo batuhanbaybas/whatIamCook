f393c59c9c8737fedbfa4ac21934d7c01cfb9f4f
batuhanbaybas <b.baybas@gmail.com>
2021-10-09 20:20:18 +0300

    .env

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {View, FlatList} from 'react-native';
-// import Config from 'react-native-config';
+import Config from 'react-native-config';
 import CategoryList from '../../Components/CategoryList/CategoryList';
 import useFetchCategory from '../../Hooks/FetchCategory/useFetchCategory';
 import {styles} from './category.style';
 
 const Category = () => {
-  const ALL_CATEGORY_API_URL =
-    'https://www.themealdb.com/api/json/v1/1/categories.php';
-  //fetch hook u çğırıldı
-  const {data} = useFetchCategory(ALL_CATEGORY_API_URL);
+  //fetch custom hook u çağırıldı
+  const {data} = useFetchCategory(Config.ALL_CATEGORY_API_URL);
   console.log(data);
   const renderCategory = ({item}) => <CategoryList category={item} />;
 
