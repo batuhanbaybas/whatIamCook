856b6e0cc754ab397ef54ce4d34f6a4f762fba01
batuhanbaybas <b.baybas@gmail.com>
2021-10-09 19:43:51 +0300

    :)

--- src/Components/CategoryList/CategoryList.js
+++ src/Components/CategoryList/CategoryList.js
@@ -3,8 +3,6 @@ import {View, Text} from 'react-native';
 import {styles} from './categoryList.style';
 
 const CategoryList = ({category}) => {
-  console.log(`katogori: ${category}`);
-  console.log('bişiler');
   return (
     <View style={styles.container}>
       <Text>{category.strCategory}</Text>
